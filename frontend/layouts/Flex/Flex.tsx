5234bc136ed317d33b6470f8c7b11f3f528d03af
BaurinAnton <baurinanton2013@yandex.ru>
2022-12-12 21:16:46 +0300

    feat(frontend): implemented Flex
    component layout Flex is implemented

--- frontend/layouts/Flex/Flex.tsx
+++ frontend/layouts/Flex/Flex.tsx
@@ -5,16 +5,44 @@ type TProps = {
     justifyContent?: "space-between" | "flex-start" | "flex-end" | "center"
     alignItems?: "flex-start" | "flex-end" | "center"
     flexDirection?: "row" | "column"
+    flexWrap?: "wrap" | "nowrap"
+    columnGap?: string
+    rowGap?: string
+    padding?: string
+    margin?: string
+
+    className?: string
+    onClick?: () => void
 }
 
-export const Flex = ({ children, justifyContent, alignItems, flexDirection }: TProps) => {
+export const Flex = ({
+    children,
+    justifyContent,
+    alignItems,
+    flexDirection,
+    flexWrap,
+    columnGap,
+    rowGap,
+    className,
+    onClick,
+    padding,
+    margin
+}: TProps) => {
     return (
-        <div className={style.flex} style={
-            {
-                justifyContent: justifyContent,
-                alignItems: alignItems,
-                flexDirection: flexDirection
-            }}>
+        <div
+            className={`${style.flex} ${className}`}
+            onClick={onClick}
+            style={
+                {
+                    justifyContent: justifyContent,
+                    alignItems: alignItems,
+                    flexDirection: flexDirection,
+                    padding: padding,
+                    margin: margin,
+                    flexWrap: flexWrap,
+                    columnGap: columnGap,
+                    rowGap: rowGap
+                }}>
             {children}
         </div>
     )
